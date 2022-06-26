53e1bbe1f6e638be88eca1b2d1f00566ae89e211
Miles Butler <miles@milesbutler.dev>
2022-06-25 22:02:46 -0700

    Add loop to not repeat letters
     - Run checks in parallel

--- src/scattergories.py
+++ src/scattergories.py
@@ -7,18 +7,27 @@ from fire import Fire
 from tqdm import tqdm
 
 LETTERS = "ABCDEFGHIJKLMNOPRSTW"
+already_done: set[str] = set()
 
 
-def main(seconds: int) -> None:
+def main() -> None:
     """Main function."""
-    print("Generating a letter")
-    for _ in tqdm(range(3)):
-        time.sleep(1)
-    letter = random.choice(LETTERS)
-    print(f"Letter: {letter}")
-    for _ in tqdm(range(seconds)):
-        time.sleep(1)
-    print("Time's up!")
+    while True:
+        print("Welcome to Scattergories! Enter # of seconds and press enter to start.")
+        try:
+            seconds = int(input("Seconds: "))
+        except ValueError:
+            print("Invalid input.")
+            continue
+        letter = random.choice(list(set(LETTERS) - already_done))
+        already_done.add(letter)
+        print(f"Letter: {letter}")
+        try:
+            for _ in tqdm(range(seconds)):
+                time.sleep(1)
+            print("Time's up!")
+        except KeyboardInterrupt:
+            print("Round canceled.")
 
 
 if __name__ == "__main__":
