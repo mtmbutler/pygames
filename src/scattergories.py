4da64737ed45bbc5d62b2df42a6f6493c1931f4e
Miles Butler <miles@milesbutler.dev>
2023-01-01 14:00:05 -0800

    Add scattergories prompts

--- src/scattergories.py
+++ src/scattergories.py
@@ -2,32 +2,46 @@
 
 import random
 import time
+from pathlib import Path
 
 from fire import Fire
 from tqdm import tqdm
 
+PROMPTS_PATH = Path(__file__).parent / "scattergories.txt"
 LETTERS = "ABCDEFGHIJKLMNOPRSTW"
-already_done: set[str] = set()
+NUM_PROMPTS = 12
+SECONDS_PER_ROUND = 180
+SEP = "==="
 
 
 def main() -> None:
     """Main function."""
+    print("Welcome to Scattergories!")
+
+    # Set up letters and prompts
+    with open(PROMPTS_PATH, encoding="utf-8") as f:
+        prompts = [ln.strip() for ln in f.readlines()]
+    letters = list(LETTERS)
+    random.shuffle(prompts)
+    random.shuffle(letters)
+
     while True:
-        print("Welcome to Scattergories! Enter # of seconds and press enter to start.")
-        try:
-            seconds = int(input("Seconds: "))
-        except ValueError:
-            print("Invalid input.")
-            continue
-        letter = random.choice(list(set(LETTERS) - already_done))
-        already_done.add(letter)
+        input("Press enter to start a round. Press Ctrl-C to end the round early.")
+        print(SEP)
+        letter = letters.pop()
         print(f"Letter: {letter}")
+        print("Prompts:")
+        for i in range(NUM_PROMPTS):
+            print(f"  {i + 1}. {prompts.pop()}")
+        print("")
         try:
-            for _ in tqdm(range(seconds)):
+            for _ in tqdm(range(SECONDS_PER_ROUND)):
                 time.sleep(1)
             print("Time's up!")
         except KeyboardInterrupt:
             print("Round canceled.")
+        finally:
+            print(SEP)
 
 
 if __name__ == "__main__":
